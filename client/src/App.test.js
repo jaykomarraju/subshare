4493d10b2172d95724c363528cf66218c5974bab
Jayanth Komarraju <jay.komarraju@gmail.com>
2025-02-19 18:10:06 -0500

    updated client tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Subshare brand in the navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const brandElements = screen.getAllByText(/Subshare/i);
+  expect(brandElements.length).toBeGreaterThan(0);
 });
